beb49566c0616e6b239b51338b947b92513a3d88
sheep8888 <92249359+sheep8888@users.noreply.github.com>
2024-03-03 21:05:28 +0800

    Create lstm.py

---
+++ model/lstm.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+class LSTMModel(nn.Module):
+    def __init__(self, vocab_size, num_hiddens=512, num_layers=1, **kwargs):
+        super(LSTMModel, self).__init__(**kwargs)
+        self.vocab_size = vocab_size
+        self.num_hiddens = num_hiddens
+        self.lstm = nn.LSTM(vocab_size, num_hiddens, num_layers)
+        self.linear = nn.Linear(self.num_hiddens, self.vocab_size)
+        
+    def forward(self, X, state):
+        # 这里简单地使用one hot编码（也可使用更好的词嵌入）
+        X = F.one_hot(X.T.long(), self.vocab_size)
+        X = X.to(torch.float32)
+        Y, state = self.lstm(X, state)
+        output = self.linear(Y.reshape((-1, Y.shape[-1])))
+        return output, state
+    
+    def init_state(self, batch_size, device):
+        return (torch.zeros((self.lstm.num_layers, batch_size, self.num_hiddens),device=device),
+                torch.zeros((self.lstm.num_layers, batch_size, self.num_hiddens),device=device))
